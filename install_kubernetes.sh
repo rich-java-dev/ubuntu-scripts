03adaf30029299d3b3181ac5dd80b98b686d4ea7
Rich White <32989822+rich-java-dev@users.noreply.github.com>
2021-12-09 14:09:49 -0500

    Create install_kubernetes.sh

---
+++ install_kubernetes.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#become root
+# disable firewall
+# ufw disable
+
+# disable swap
+swapoff -a; sed -i '/swap/d' /etc/fstab
+
+# update sysctl
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+
+# install docker
+apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update
+apt install -y docker-ce=5:19.03.10~3-0~ubuntu-focal containerd.io
+
+# configure systemd cgroup for docker
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+systemctl enable docker
+systemctl daemon-reload
+systemctl restart docker
+
+# install kubernetes
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+apt update && apt install -y kubeadm=1.18.5-00 kubelet=1.18.5-00 kubectl=1.18.5-00
